fb80ceb4594212321994bdbaa5bdb0cda91edc1d
Eric Wenaas <eric.wenaas@bdeb.qc.ca>
2023-09-07 22:46:02 -0400

    Version remise aux étudiants
    Ajout d'une inclusion pour la classe contenant les constantes.

--- ThreadLivraison/Program.cs
+++ ThreadLivraison/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static ThreadLivraison.ConstantesSimulation;
+
+Console.WriteLine(NOMBRE_COMMANDES);
